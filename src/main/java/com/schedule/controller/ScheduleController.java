dab53557882a43d0ccac2c1eadac9d8358a19777
0younge <s45628439@gmail.com>
2026-04-11 13:40:29 +0900

    feat: GetMapping

--- src/main/java/com/schedule/controller/ScheduleController.java
+++ src/main/java/com/schedule/controller/ScheduleController.java
@@ -2,13 +2,14 @@ package com.schedule.controller;
 
 import com.schedule.dto.createScheduleRequest;
 import com.schedule.dto.createScheduleResponse;
+import com.schedule.dto.getScheduleResponse;
 import com.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +20,19 @@ public class ScheduleController {
     @PostMapping("/schedules")
     public ResponseEntity<createScheduleResponse> createSchedule(@RequestBody createScheduleRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.create(request));
+    }
+
+    @GetMapping("/schedules/{id}")
+    public ResponseEntity<getScheduleResponse> getOneSchedule(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getOne(id));
+    }
+
+    @GetMapping("/schedules")
+    public ResponseEntity<List<getScheduleResponse>> getAllSchedule(@RequestParam(required = false) String writer) {
+        if (writer != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getAllByWriter(writer));
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getAll());
     }
 
 }
--- src/main/java/com/schedule/service/ScheduleService.java
+++ src/main/java/com/schedule/service/ScheduleService.java
@@ -2,11 +2,15 @@ package com.schedule.service;
 
 import com.schedule.dto.createScheduleRequest;
 import com.schedule.dto.createScheduleResponse;
+import com.schedule.dto.getScheduleResponse;
 import com.schedule.entity.Schedule;
 import com.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,5 +35,46 @@ public class ScheduleService {
                 schedule.getModifiedAt());
     }
 
+    @Transactional(readOnly = true)
+    public getScheduleResponse getOne(Long id) {
+        Schedule schedule = scheduleRepository.findById(id).orElseThrow(() -> new IllegalStateException("없는 일정입니다."));
+        return new getScheduleResponse(
+                schedule.getId(),
+                schedule.getTitle(),
+                schedule.getContent(),
+                schedule.getWriter(),
+                schedule.getCreatedAt(),
+                schedule.getModifiedAt());
+    }
+
+    @Transactional(readOnly = true)
+    public List<getScheduleResponse> getAll() {
+        return scheduleRepository.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Schedule::getCreatedAt).reversed())
+                .map(schedule -> new getScheduleResponse(
+                        schedule.getId(),
+                        schedule.getTitle(),
+                        schedule.getContent(),
+                        schedule.getWriter(),
+                        schedule.getCreatedAt(),
+                        schedule.getModifiedAt()))
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<getScheduleResponse> getAllByWriter(String writer) {
+        return scheduleRepository.findAllByWriter(writer)
+                .stream()
+                .sorted(Comparator.comparing(Schedule::getCreatedAt).reversed())
+                .map(schedule -> new getScheduleResponse(
+                        schedule.getId(),
+                        schedule.getTitle(),
+                        schedule.getContent(),
+                        schedule.getWriter(),
+                        schedule.getCreatedAt(),
+                        schedule.getModifiedAt()))
+                .toList();
+    }
 
 }

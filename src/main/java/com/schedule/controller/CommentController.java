74fbe7077a7d4af6ebb9a76972cb693da9277f6d
0younge <s45628439@gmail.com>
2026-04-13 15:28:47 +0900

    style: add annotation
    -컨트롤러 부분에 기능 주석을 추가했습니다.

--- src/main/java/com/schedule/controller/CommentController.java
+++ src/main/java/com/schedule/controller/CommentController.java
@@ -17,6 +17,15 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    /**
+     * 댓글 추가 API :
+     *
+     * 일정 번호에 접근하여 댓글을 작성
+     *
+     * @param id 일정 번호
+     * @param request 댓글 내용, 작성자, 비밀번호 입력
+     * @return http 상태코드와 id, 내용, 작성자, 작성/수정 일자 반환
+     */
     @PostMapping("/schedules/{id}/comments")
     public ResponseEntity<CreateCommentResponse> createComment(@PathVariable Long id, @RequestBody CreateCommentRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.create(id, request));
--- src/main/java/com/schedule/controller/ScheduleController.java
+++ src/main/java/com/schedule/controller/ScheduleController.java
@@ -16,7 +16,11 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     /**
-     * 일정 추가API
+     * 일정 추가 API:
+     * 제목, 내용 작성자, 비밀번호를 입력받아 일정을 추가
+     *
+     * @param request 제목, 내용, 작성자, 비밀번호 입력
+     * @return 일정번호, 제목, 내용, 작성자, 작성/수정일 반환
      */
     @PostMapping("/schedules")
     public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
@@ -24,15 +28,24 @@ public class ScheduleController {
     }
 
     /**
-     * 일정 조회API
+     * 단건 조회 API:
+     * 일정번호로 단건을 조회
+     *
+     * @param id 조회할 일정 번호
+     * @return 일정번호, 제목, 내용, 작성자, 작성/수정일 댓글 반환
      */
-    // 단건 조회
     @GetMapping("/schedules/{id}")
     public ResponseEntity<GetOneScheduleResponse> getOneSchedule(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.getOne(id));
     }
 
-    // 전체 조회
+    /**
+     * 전체 조회 API:
+     * 작성자를 Param으로 받으면 작성자가 작성한 일정 전체 조회 / 아닐 시 전체 일정 조회
+     *
+     * @param writer 조회할 작성자 입력
+     * @return 일정번호, 제목, 내용, 작성자, 작성/수정일 반환
+     */
     @GetMapping("/schedules")
     public ResponseEntity<List<GetScheduleResponse>> getAllSchedule(@RequestParam(required = false) String writer) {
         if (writer != null) {
@@ -42,7 +55,12 @@ public class ScheduleController {
     }
 
     /**
-     * 일정 수정API
+     * 일정 수정 API:
+     * 수정할 일정 번호로 접근해 비밀번호 일치시 제목, 작성자 변경
+     *
+     * @param id 수정할 일정 번호 입력
+     * @param request 수정할 제목, 작성자, 인증을 위한 비밀번호 입력
+     * @return 일정번호, 제목, 내용, 작성자, 작성/수정일 반환
      */
     @PutMapping("/schedules/{id}")
     public ResponseEntity<UpdateScheduleResponse> updateSchedule(@PathVariable Long id, @RequestBody UpdateScheduleRequest request) {
@@ -50,7 +68,11 @@ public class ScheduleController {
     }
 
     /**
-     * 일정 삭제API
+     * 일정 삭제 API:
+     * 삭제할 일정번호로 접근해 일정 삭제
+     *
+     * @param id 삭제할 일정 번호 입력
+     * @return 삭제 완료 HttpStatus코드만 반환
      */
     @DeleteMapping("/schedules/{id}")
     public ResponseEntity<Void> deleteSchedule(@PathVariable Long id) {

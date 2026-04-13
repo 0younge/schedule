104ea263640c768e093fbdc76f9dc8d4424be417
0younge <s45628439@gmail.com>
2026-04-13 10:36:36 +0900

    feat: CommentService

---
+++ src/main/java/com/schedule/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.schedule.service;
+
+import com.schedule.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+}
